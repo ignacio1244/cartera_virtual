c7e26ab1ee41bc830a028fb95150e35284cc68b0
Fvega <fvegamaza@gmail.com>
2021-07-24 14:54:48 -0300

    permisos?

--- bot.py
+++ bot.py
@@ -1,17 +1,3 @@
 from telegram.ext import  Updater, CommandHandler
 
-def start(update, context):
-    update.message.reply_text("hola hola")
 
-
-if __name__ == "__main__":
-    Updater = Updater(token="1874222275:AAHJu-ptKk8yacJ8V-jR4FKUF6dvSylugXk", use_context = True )
-
-    dp = Updater.dispatcher
-
-    dp.add_handler(CommandHandler("start", start))
-
-    Updater.start_polling()
-    Updater.idle()    
-    UWU
-    uwu de permiso
